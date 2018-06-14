c6d242a2c2a90774f17d13c3196ff479a51b913a
W56998 <w56998@student.wsiz.rzeszow.pl>
2018-06-14 18:58:33 +0200

    Wyświetlenie listy

--- app/src/main/java/com/example/stud/appmusic/searchalbum/SearchAlbumActivity.java
+++ app/src/main/java/com/example/stud/appmusic/searchalbum/SearchAlbumActivity.java
@@ -4,8 +4,12 @@ import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +19,11 @@ import com.example.stud.appmusic.R;
 import com.example.stud.appmusic.api.ApiService;
 import com.example.stud.appmusic.api.SearchAlbum;
 import com.example.stud.appmusic.api.SearchAlbums;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -26,18 +35,25 @@ public class SearchAlbumActivity extends AppCompatActivity {
     RecyclerView rvList;
 
     SharedPreferences sharedPreferences;
+    List<SearchAlbum> albums = new ArrayList<>( 0 );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_album);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled( true );
-
         sharedPreferences = getPreferences(MODE_PRIVATE);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled( true );
 
         etQuery = findViewById(R.id.etQuery);
         rvList = findViewById(R.id.rvList);
+
+        SearchAlbumAdapter searchAlbumAdapter = new SearchAlbumAdapter(albums);
+        rvList.setAdapter(searchAlbumAdapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager( this );
+        rvList.setLayoutManager(linearLayoutManager);
 
         try {
             etQuery.setText( sharedPreferences.getString( "query" , null ));
@@ -80,6 +96,7 @@ public class SearchAlbumActivity extends AppCompatActivity {
             @Override
             public void onResponse(@NonNull Call<SearchAlbums> call, @NonNull
                     Response<SearchAlbums> response) {
+
                 SearchAlbums searchAlbums = response.body();
                 if (searchAlbums == null || searchAlbums. album == null ||
                         searchAlbums. album .isEmpty()) {
@@ -87,9 +104,11 @@ public class SearchAlbumActivity extends AppCompatActivity {
                             Toast. LENGTH_SHORT ).show();
                     return;
                 }
-                Toast. makeText (SearchAlbumActivity. this , "Znaleziono " +
-                        searchAlbums. album .size() + " wyników" , Toast. LENGTH_SHORT ).show();
+
+                updateList(searchAlbums);
+
             }
+
             @Override
             public void onFailure( @NonNull Call<SearchAlbums> call, Throwable t) {
                 Toast. makeText (SearchAlbumActivity. this , "Błąd pobierania danych: " +
@@ -98,12 +117,92 @@ public class SearchAlbumActivity extends AppCompatActivity {
         });
     }
 
-
-
+    private void updateList(SearchAlbums searchAlbums) {
+        albums.clear();
+        albums.addAll(searchAlbums.album );
+        rvList.getAdapter().notifyDataSetChanged();
+    }
 
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true ;
     }
-}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu. sort_albums_menu , menu);
+        return true ;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemSortName:
+                sortByName();
+                return true;
+            case R.id.itemSortYear:
+                sortByYear();
+                return true;
+            case R.id.itemSortSales:
+                sortBySales();
+                return true;
+            case R.id.itemSortScore:
+                sortByScore();
+                return true;
+            default:
+
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    private void sortByName() {
+        Collections.sort (albums , new Comparator<SearchAlbum>() {
+            @Override
+            public int compare(SearchAlbum album1, SearchAlbum album2) {
+                if (album1. strAlbum .equals(album2. strAlbum )) {
+                    return 0 ;
+                }
+                return album1. strAlbum .compareTo(album2. strAlbum );
+            }
+        });
+        rvList.getAdapter().notifyDataSetChanged();
+    }
+    private void sortByYear() {
+        Collections.sort (albums , new Comparator<SearchAlbum>() {
+            @Override
+            public int compare(SearchAlbum album1, SearchAlbum album2) {
+                if (album1.intYearReleased == album2.intYearReleased ) {
+                    return 0 ;
+                }
+                return album1.intYearReleased < album2.intYearReleased ? - 1 : 1 ;
+            }
+        });
+        rvList.getAdapter().notifyDataSetChanged();
+    }
+    private void sortBySales() {
+        Collections.sort ( albums , new Comparator<SearchAlbum>() {
+            @Override
+            public int compare(SearchAlbum album1, SearchAlbum album2) {
+                if (album1.intSales == album2.intSales ) {
+                    return 0 ;
+                }
+                return album1.intSales < album2.intSales ? - 1 : 1 ;
+            }
+        });
+        rvList.getAdapter().notifyDataSetChanged();
+    }
+    private void sortByScore() {
+        Collections.sort ( albums , new Comparator<SearchAlbum>() {
+            @Override
+            public int compare(SearchAlbum album1, SearchAlbum album2) {
+                if (album1.intScore == album2.intScore ) {
+                    return 0;
+                }
+                return album1.intScore < album2.intScore ? - 1 : 1 ;
+            }
+        });
+        rvList.getAdapter().notifyDataSetChanged();
+    }
+
+    }
+
